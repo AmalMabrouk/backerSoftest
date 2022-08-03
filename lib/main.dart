0c039ef458b6ed0bac035692c0d5760ecdfd79a3
AmalMabrouk <amal.mabrouk@esprit.tn>
2022-08-03 17:18:38 +0200

    check files

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:bakersofttest/blocs/favorite_bloc/favorite_bloc.dart';
+import 'package:bakersofttest/blocs/shopping_cart_bloc/shopping_bloc.dart';
+import 'package:bakersofttest/views/products_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider(
+              create: (context) => ShoppingBloc()..add(LoadShoppingEvent())),
+          BlocProvider(
+              create: (context) =>
+              FavoriteBloc()
+                ..add(LoadFavoriteEvent()))
+        ],
+        child: const MaterialApp(
+            debugShowCheckedModeBanner: false, home: ProductsView()));
+  }
+}
